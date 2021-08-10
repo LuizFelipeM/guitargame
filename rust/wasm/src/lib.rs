3ab9a47e6b28a923cc7c2564afc010a4c490c141
LuizFelipeM <muniz.mourafelipe@gmail.com>
2021-08-10 19:11:47 -0300

    Changes to lib/main rust files

--- rust/wasm/src/lib.rs
+++ rust/wasm/src/lib.rs
@@ -1,8 +1,5 @@
 use wasm_bindgen::prelude::*;
 
-struct Health(i32);
-struct Name(&'static str);
-
 #[wasm_bindgen]
 pub fn add_two_ints(a: u32, b: u32) -> u32 {
     a + b
--- rust/wasm/src/main.rs
+++ rust/wasm/src/main.rs
@@ -1,4 +1,14 @@
+// struct Health(i32);
+// struct Name(&'static str);
+
+struct Player {
+    Name: &'static str,
+    Health: i32
+}
+
 fn main() {
     let value = 123;
+    let a = String::from("Test");
+    
     println!("Hello, world! {}", value);
 }
